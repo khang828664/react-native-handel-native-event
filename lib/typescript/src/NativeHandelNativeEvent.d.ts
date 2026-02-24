84450e0ef260788e3b6e2a792d1d93257feb5754
khangluu <luuhoangkhang.93@gmail.com>
2026-02-24 13:38:16 +0700

    feat(native-event): add NativeHandelNativeEvent module and related TypeScript definitions

---
+++ lib/typescript/src/NativeHandelNativeEvent.d.ts
@@ -0,0 +1,7 @@
+import { type TurboModule } from 'react-native';
+export interface Spec extends TurboModule {
+    syncUIRender(): Promise<boolean>;
+}
+declare const _default: Spec;
+export default _default;
+//# sourceMappingURL=NativeHandelNativeEvent.d.ts.map
---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,2 @@
+export declare function syncUIRender(): Promise<boolean>;
+//# sourceMappingURL=index.d.ts.map
